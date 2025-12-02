c1eff34441bda52fb2af0a4c519e2fd16bb25399
Tivoli <mhdhazem.sabbagh@gmail.com>
2025-12-02 15:18:40 +0300

    added repo layer

---
+++ src/main/java/Tivoli/Onboarding/NewHires/NewHireRepository.java
@@ -0,0 +1,6 @@
+package Tivoli.Onboarding.NewHires;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NewHireRepository extends JpaRepository {
+}
---
+++ src/main/java/Tivoli/Onboarding/NewHires/NewHires.java
@@ -0,0 +1,63 @@
+package Tivoli.Onboarding.NewHires;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDate;
+
+@Entity
+@Table (name = "New_Hires")
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class NewHires {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = true)
+    private String middleName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(unique = true, nullable = false, length = 6)
+    private Integer code;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String position;
+
+    @Column(nullable = false)
+    private String department;
+
+    @Column(nullable = false)
+    private String manager;
+
+    @Column(name = "date_of_joining", nullable = false)
+    private LocalDate dateOfJoining;
+
+    @Column(nullable = false)
+    private String degree;
+
+    private String photo;
+
+
+
+
+
+
+
+
+
+}
